f65eb54ce8c35be65bee44d2c661d0d9d6b7cb45
YiMiaoSL <2492695298@qq.com>
2025-04-28 18:28:27 +0800

    Site updated: 2025-04-28 18:28:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/23/航空订票系统-课程设计/","2024/05/23/第一篇文章/"];function toRandomPost(){
+var posts=["2024/05/23/第一篇文章/","2024/05/23/航空订票系统-课程设计/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
