09e55b32949075fa3dd18fa72f15418caa522417
YiMiaoSL <2492695298@qq.com>
2025-04-29 08:40:48 +0800

    Site updated: 2025-04-29 08:40:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/23/第一篇文章/","2025/04/04/project2016安装与office 365冲突问题/","2024/05/23/航空订票系统-课程设计/"];function toRandomPost(){
+var posts=["2024/05/23/第一篇文章/","2024/05/23/航空订票系统-课程设计/","2025/04/04/project2016安装与office 365冲突问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
